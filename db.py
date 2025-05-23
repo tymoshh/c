482fd799a8794dfe000ca549de5fbb7e10f2fd66
tymoshh <dev@tymosh.ovh>
2025-05-23 11:05:43 +0200

    test mysql

--- db.py
+++ db.py
@@ -1,3 +1,19 @@
+import os
 import mysql.connector
+
+dbcon = None
+
+def initiateConnection():
+    try:
+        global dbcon
+        dbcon = mysql.connector.connect(
+            host=os.getenv("DB_HOST"), 
+            user=os.getenv("DB_USERNAME"), 
+            password=os.getenv("DB_PASSWORD"), 
+            database=os.getenv("DB_USERNAME")
+        )
+        return 0
+    except:
+        return 1
 
 print("test")
