354a0d0369ae5b621e1034767546efad3ee4deb3
tymoshh <dev@tymosh.ovh>
2025-05-23 11:11:58 +0200

    dalsze testy db

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ dbcon = None
 
 def initiateConnection():
     try:
+        print(os.getenv("DB_HOST"))
         global dbcon
         dbcon = mysql.connector.connect(
             host=os.getenv("DB_HOST"), 
