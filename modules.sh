158e9f21d5a7fc1d8d3d54b410c784ebbc64c866
tymoshh <dev@tymosh.ovh>
2025-05-26 11:42:40 +0200

    chyba smiga db

--- modules.sh
+++ modules.sh
@@ -1,2 +1,4 @@
 export PYTHONPATH=$HOME/local/usr/lib/python3/dist-packages:$PYTHONPATH
+
 cp ./dbcon/dbcon.py /home/k24_c/cebularz7/local/usr/lib/python3/dist-packages
+chmod 700 /home/k24_c/cebularz7/local/usr/lib/python3/dist-packages/dbcon.py
