49323b43ae5e5b92f2f77045d470b94f550b0e64
tymoshh <dev@tymosh.ovh>
2025-05-29 11:34:50 +0200

    upgrade dbcon

--- dbcon/dbcon.py
+++ dbcon/dbcon.py
@@ -2,14 +2,56 @@ import os
 import hashlib
 from dotenv import load_dotenv
 import mysql.connector
+import random
 
 load_dotenv("/home/k24_c/cebularz7/.c/.env")
 
 dbcon = None
 
+class userClass:
+    def __init__(self, id, password):
+        self.id = id
+        self.password = password
+    
+    def createPasswordHash(self):
+        self.passwordHash = getHash(self.password)
+
+    def fetchToken(self):
+        initiateConnection()
+        cursor = dbcon.cursor()
+        query = "SELECT passwordhash, token FROM usertable WHERE id = %s"
+        cursor.execute(query, (self.id,))
+        result = cursor.fetchone()
+        if result:
+            stored_passwordhash, fetchedtoken = result
+            if stored_passwordhash == self.passwordHash:
+                closeConnection()
+                self.token = fetchedtoken
+                return 0
+            else:
+                closeConnection()
+                return 1
+        else:
+            closeConnection()
+            return 1
+
+    def createToken(self):
+        self.token = getHash(str(random.randint()))
+
+    def createUser(self):
+        initiateConnection()
+        cursor = dbcon.cursor()
+        query = """
+        INSERT INTO users (id, passwordHash, token)
+        VALUES (%s, %s, %s)
+        """
+        values = (self.id, self.passwordHash, self.token)
+        dbcon.cursor().execute(query, values)
+        dbcon.commit()
+        closeConnection()
+
 def initiateConnection():
     try:
-        print(os.getenv("DB_PASSWORD"))
         global dbcon
         dbcon = mysql.connector.connect(
             host=os.getenv("DB_HOST"), 
@@ -25,8 +67,6 @@ def closeConnection():
     global dbcon
     dbcon.close()
 
-def getHash(password):
-    return hashlib.sha256(password.encode()).hexdigest()
+def getHash(mystring):
+    return hashlib.sha256(mystring.encode()).hexdigest()
 
-def getToken(username, passwordHash):
-    print("TEST")
---
+++ usrmgr/usrmgr.py
@@ -0,0 +1,18 @@
+import dbcon
+
+ID = None
+PASSWD = None
+
+print("DODAWANIE UZYTKOWNIKOW DO KASYNA")
+ID = input("Podaj ID : ")
+PASSWD = input("Podaj haslo : ")
+
+userObject = dbcon.userClass(ID, PASSWD)
+userObject.createPasswordHash()
+userObject.createToken()
+userObject.createUser()
+
+print()
+print("Uzytkownik dodany!")
+print()
+input("Nacisnij enter, aby kontynuowac ...")
