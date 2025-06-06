0d83ead644441c39e875758b06bc119e7e0f33b6
tymoshh <dev@tymosh.ovh>
2025-06-06 09:12:49 +0200

    automatyczny chmod

--- modules.sh
+++ modules.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 export PYTHONPATH=$HOME/local/usr/lib/python3/dist-packages:$PYTHONPATH
 
 cp ./dbcon/dbcon.py /home/k24_c/cebularz7/local/usr/lib/python3/dist-packages
 chmod 700 /home/k24_c/cebularz7/local/usr/lib/python3/dist-packages/dbcon.py
+
+find ./ -type d -exec chmod 711 {} \;
+find ./ ! -name "*.php" ! -type d -exec chmod 644 {} \;
+find ./ -name '*.php' -exec chmod 700 {} \;
+find ./ -name '*.php' -exec dirname {} \; | xargs -I {} find {} -type f -exec chmod 600 {} \;
